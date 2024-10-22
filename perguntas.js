2525ea1d6684dedd548e829924c1f70abfbb4137
gabylinda10 <161468537+gabylinda10@users.noreply.github.com>
2024-10-22 10:05:59 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,20 @@
+criaCartao(
+    'geografia',
+    'qual é o menor pais do mundo',
+    'vaticano'
+)
+criaCartao(
+    'geografia',
+    'qual é o maior pais do mundo ?',
+    'russia'
+)
+criaCartao(
+    'maquiagem',
+    'como utiliza a blindgem?',
+    'a blindagem deve ser utilizada nas areas mais faceis da maquiagem sair'
+)
+criaCartao(
+    'maquiagem',
+    'qual produto é utilizado para fixar a maquiagem?',
+    'spray fixador'
+)
